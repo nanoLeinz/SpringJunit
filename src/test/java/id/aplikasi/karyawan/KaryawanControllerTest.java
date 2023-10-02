678431484630647568d2fb6a86f304e142fa9a55
nano <nanoleinz@gmail.com>
2023-10-02 20:45:45 +0700

    Added Initialization

--- src/test/java/id/aplikasi/karyawan/KaryawanControllerTest.java
+++ src/test/java/id/aplikasi/karyawan/KaryawanControllerTest.java
@@ -3,8 +3,8 @@ package id.aplikasi.karyawan;
 import id.aplikasi.karyawan.controller.KaryawanController;
 import id.aplikasi.karyawan.model.KaryawanModel;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,19 +14,21 @@ import static org.junit.Assert.assertNotNull;
 
 @SpringBootTest
 class KaryawanControllerTest {
-    private KaryawanController controller = new KaryawanController();
-    @Before
+    private KaryawanController controller;
+    @BeforeEach
     public void setUp() {
         controller = new KaryawanController();
     }
 
     @Test
+    @DisplayName("Test Tambah Karyawan")
     public void testTambahKaryawan() {
         controller.tambahKaryawan(1, "John Doe", "Manager");
-        assertEquals(3, controller.semuaKaryawan().size());
+        assertEquals(1, controller.semuaKaryawan().size());
     }
 
     @Test
+    @DisplayName("Test Get Karyawan by Id")
     public void testKaryawanById() {
         controller.tambahKaryawan(1, "Jane Doe", "Developer");
         KaryawanModel karyawan = controller.karyawanById(1);
@@ -36,6 +38,7 @@ class KaryawanControllerTest {
     }
 
     @Test
+    @DisplayName("Test Perbarui Karyawan")
     public void testPerbaruiKaryawan() {
         controller.tambahKaryawan(1, "John Doe", "Manager");
         controller.perbaruiKaryawan(1, "John Smith", "Director");
@@ -45,12 +48,13 @@ class KaryawanControllerTest {
     }
 
     @Test
+    @DisplayName("Test Hapus Karyawan")
     public void testHapusKaryawan() {
         controller.tambahKaryawan(1, "John Doe", "Manager");
         controller.hapusKaryawan(1);
         assertEquals(0, controller.semuaKaryawan().size());
     }
-    @After
+    @AfterEach
     public void tearDown() {
         controller = null;
     }
